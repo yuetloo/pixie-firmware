62ce70d5a269f0b262178574f2d22ae53183869b
yuetloo <contact@yuetloo.com>
2025-02-28 12:46:55 -0500

    add missing esp components

--- esp32c3/build.mk
+++ esp32c3/build.mk
@@ -7,6 +7,8 @@ CFLAGS      ?= -W -Wall -Wextra -Werror -Wundef -Wshadow -pedantic \
                -march=rv32imc -mabi=ilp32 \
                -Os -ffunction-sections -fdata-sections \
                -I. -I$(MDK)/$(ARCH) \
+               -I/root/.espressif/components/esp_hw_support/include \
+               -I/root/.espressif/components/esp_driver_gpio/include \
                -I/root/.espressif/components/freertos/FreeRTOS-Kernel/include \
                -I/root/.espressif/components/log/include \
                -I$(MDK)/components/crypto/crypto \
@@ -16,14 +18,12 @@ CFLAGS      ?= -W -Wall -Wextra -Werror -Wundef -Wshadow -pedantic \
 LINKFLAGS   ?= -T$(MDK)/$(ARCH)/link.ld -nostdlib -nostartfiles -Wl,--gc-sections $(EXTRA_LINKFLAGS)
 CWD         ?= $(realpath $(CURDIR))
 FLASH_ADDR  ?= 0  # 2nd stage bootloader flash offset
-DOCKER      ?= docker run -it --rm -v $(CWD):$(CWD) -v $(MDK):$(MDK) -w $(CWD) mdashnet/riscv
-TOOLCHAIN   ?= $(DOCKER) riscv-none-elf
 SRCS        ?= $(MDK)/$(ARCH)/boot.c $(SOURCES)
 
 build: $(PROG).bin
 
 $(PROG).elf: $(SRCS)
-	$(TOOLCHAIN)-gcc  $(CFLAGS) $(SRCS) $(LINKFLAGS) -o $@
+	gcc  $(CFLAGS) $(SRCS) $(LINKFLAGS) -o $@
 #	$(TOOLCHAIN)-size $@
 
 $(PROG).bin: $(PROG).elf $(ESPUTIL)
