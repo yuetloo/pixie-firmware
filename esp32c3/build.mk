911f888ca4e2a03b6170b6a815859e74c7d5b06f
yuetloo <contact@yuetloo.com>
2025-03-10 13:34:39 -0400

    add more include path

--- esp32c3/build.mk
+++ esp32c3/build.mk
@@ -2,20 +2,50 @@ PROG        ?= firmware
 ARCH        ?= esp32c3
 MDK         ?= $(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 ESPUTIL     ?= $(MDK)/esputil/esputil
-CFLAGS      ?= -W -Wall -Wextra -Werror -Wundef -Wshadow -pedantic \
-               -Wdouble-promotion -fno-common -Wconversion \
+CFLAGS      ?= -W \
+               -fno-common \
                -march=rv32imc -mabi=ilp32 \
                -Os -ffunction-sections -fdata-sections \
                -I. -I$(MDK)/$(ARCH) \
                -I$(MDK) \
-               -I/root/.espressif/components/esp_hw_support/include \
-               -I/root/.espressif/components/esp_driver_gpio/include \
+               -I/root/.espressif/riscv32-esp-elf/riscv32-esp-elf/include \
+               -I/root/.espressif/components/esp_driver_spi/include \
+               -I/root/.espressif/components/newlib/platform_include \
+               -I/root/.espressif/components/bt/porting/npl/freertos/include \
+               -I/root/.espressif/components/bt/host/nimble/port/include \
+               -I/root/.espressif/components/bt/host/nimble/nimble/nimble/include \
+               -I/root/.espressif/components/bt/host/nimble/nimble/nimble/host/include \
+               -I/root/.espressif/components/bt/host/nimble/nimble/porting/nimble/include \
+               -I/root/.espressif/components/bt/host/nimble/nimble/nimble/transport/include \
+               -I/root/.espressif/components/bt/host/nimble/nimble/nimble/host/util/include \
+               -I/root/.espressif/components/bt/host/nimble/nimble/nimble/host/services/gap/include \
+               -I/root/.espressif/components/bt/host/nimble/nimble/nimble/host/services/gatt/include \
+               -I/root/.espressif/components/esp_driver_rmt/include \
+               -I/root/.espressif/components/esp_timer/include \
+               -I/root/.espressif/components/heap/include \
+               -I/root/.espressif/components/esp_system/include \
+               -I/root/.espressif/components/esp_partition/include \
+               -I/root/.espressif/components/riscv/include \
+               -I/root/.espressif/components/nvs_flash/include \
+               -I/root/.espressif/components/efuse/include \
+               -I/root/.espressif/components/efuse/esp32c3/include \
+               -I/root/.espressif/components/hal/include \
+               -I/root/.espressif/components/hal/esp32c3/include \
+               -I/root/.espressif/components/hal/platform_port/include \
+               -I/root/.espressif/components/esp_rom/include \
+               -I/root/.espressif/components/esp_rom/esp32c3/include \
+               -I/root/.espressif/components/soc/esp32c3/include \
+               -I/root/.espressif/components/soc/include \
+               -I/root/.espressif/components/freertos/config/include \
+               -I/root/.espressif/components/freertos/FreeRTOS-Kernel/portable/riscv/include/freertos \
                -I/root/.espressif/components/freertos/FreeRTOS-Kernel/include \
                -I/root/.espressif/components/freertos/config/include/freertos \
-               -I/root/.espressif/components/freertos/config/riscv/include \
+               -I/root/.espressif/components/freertos/config/include \
+               -I/root/.espressif/components/esp_hw_support/include \
+               -I/root/.espressif/components/esp_driver_gpio/include \
                -I/root/.espressif/components/log/include \
                -I/root/.espressif/components/esp_common/include \
-               -I$(MDK)/components/crypto/crypto \
+               -I$(MDK)/components/crypto \
                -I$(MDK)/components/firefly-display/include \
                -I$(MDK)/components/firefly-scene/include \
                $(EXTRA_CFLAGS)
